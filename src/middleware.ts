8651dee0ee5c17bc0f14867269f041615a0f4f98
reveuse12 <prayag129787@gmail.com>
2024-09-18 22:26:14 +0530

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = ["/login", "/signup", "/", "/verify", "/reset"].includes(
+    path
+  );
+
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/admin", "/login", "/signup", "/verify", "/reset"],
+};
